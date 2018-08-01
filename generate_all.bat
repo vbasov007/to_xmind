6ce54de34c15da0946974f4c9e14d5b78b4bd53e
Basov Vasily (IFRU) <Vasily.Basov@infineon.com>
2018-08-01 19:52:26 +0300

    more added to .bat

--- generate_all.bat
+++ generate_all.bat
@@ -1,68 +1,81 @@
-to_xmind.py datafiles\MOSFET.xlsx -i -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t13 -t17 -t5 -t1 -a5 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 500-900V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t13 -t17 -t5 -t1 -a5 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 500-900V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t17 -t13 -t5 -t1 -a5 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_Technology.xmind --main="MOSFET N Type 500-900V By Voltage, Technology"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t17 -t13 -t5 -t1 -a5 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_Technology.xmind --main="MOSFET N Type 500-900V By Voltage, Technology"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"18=Automotive" -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t13 -t5 -t1 -a5 -a17 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_500_900_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 500-900V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"18=Automotive" -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t13 -t5 -t1 -a5 -a17 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_500_900_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 500-900V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"9=N" -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t11 -t13 -t5 -t1 -a5 -a17 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_Current.xmind --main="MOSFET N Type 500-900V By Voltage, Current"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"9=N" -o"10=500 V, 600 V, 650 V, 700 V, 800 V, 900 V, 950 V" -t10 -t11 -t13 -t5 -t1 -a5 -a17 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_500_900_V_By_Voltage_Current.xmind --main="MOSFET N Type 500-900V By Voltage, Current"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"10=100 V, 120 V, 135 V, 150 V, 200 V, 220 V, 240 V, 250 V, 300 V, 400 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_100_400_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 100-400V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"10=100 V, 120 V, 135 V, 150 V, 200 V, 220 V, 240 V, 250 V, 300 V, 400 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_100_400_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 100-400V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"18=Automotive" -o"10=100 V, 120 V, 135 V, 150 V, 200 V, 220 V, 240 V, 250 V, 300 V, 400 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_100_400_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 100-400V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"18=Automotive" -o"10=100 V, 120 V, 135 V, 150 V, 200 V, 220 V, 240 V, 250 V, 300 V, 400 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_100_400_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 100-400V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"9=N" -o"10=12 V, 20 V, 24 V, 25 V, 30 V, 34 V, 40 V, 55 V, 60 V, 75 V, 80 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_12_80_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 12-80V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"9=N" -o"10=12 V, 20 V, 24 V, 25 V, 30 V, 34 V, 40 V, 55 V, 60 V, 75 V, 80 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_N_Type_12_80_V_By_Voltage_R_dson.xmind --main="MOSFET N Type 12-80V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"9=N" -o"18=Automotive" -o"10=12 V, 20 V, 24 V, 25 V, 30 V, 34 V, 40 V, 55 V, 60 V, 75 V, 80 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_12_80_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 12-80V By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"9=N" -o"18=Automotive" -o"10=12 V, 20 V, 24 V, 25 V, 30 V, 34 V, 40 V, 55 V, 60 V, 75 V, 80 V" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_N_Type_12_80_V_By_Voltage_R_dson.xmind --main="MOSFET Automotive N Type 12-80V By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -o"9=P" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_P_Type_By_Voltage_R_dson.xmind --main="MOSFET P Type By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"9=P" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_P_Type_By_Voltage_R_dson.xmind --main="MOSFET P Type By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i  -o"18=Automotive" -o"9=P" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_P_Type_By_Voltage_R_dson.xmind --main="MOSFET Automotive P Type By Voltage, R_dson"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -o"18=Automotive" -o"9=P" -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Automotive_P_Type_By_Voltage_R_dson.xmind --main="MOSFET Automotive P Type By Voltage, R_dson"
 
-to_xmind.py datafiles\MOSFET.xlsx -i -e"9=P, N" -t9 -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Exotic_Types_By_Type_Voltage_R_dson.xmind --main="MOSFET Exotic Types By Type, Voltage, R_dson"
-
-
-
-to_xmind.py datafiles\IGBT_modules.xlsx -i -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_all_Voltage_Configuration_Current.xmind --main="All IGBT Modules by Voltage and Configuration"
-
-to_xmind.py datafiles\IGBT_modules.xlsx -i -o"8=Dual" -t6 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_Dual(FF)_Voltage_Current.xmind --main="Dual(FF) IGBT Modules by Voltage and Configuration"
-
-to_xmind.py datafiles\IGBT_modules.xlsx -i -o"8=Dual,Single" -t6 -t12 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_Dual(FF)_Single(FZ)_Voltage_Technology.xmind --main="Dual(FF), Single(FZ) IGBT Modules by Voltage and Technology"
-
-to_xmind.py datafiles\IGBT_modules.xlsx -i --match="9=.XT" -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_with_dot_XT.xmind --main="IGBT Modules with dot_XT"
-
-to_xmind.py datafiles\IGBT_modules.xlsx -i --match="15=Automotive" -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_for_Automotive_Voltage_Configuration_Current.xmind --main="IGBT Modules for Automotive by Voltage and Configuration"
-
-
-to_xmind.py datafiles\IGBT_Discretes.xlsx -i -o"15=IGBT" -t7 -t12 -t11 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_without_diode_Voltage_Current.xmind --main="IGBT Discretes w/o diode by Voltage, Current, Freq range"
-
-to_xmind.py datafiles\IGBT_Discretes.xlsx -i -o"15=IGBT + Diode" -t7 -t12 -t11 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_with_diode_Voltage_Current.xmind --main="IGBT Discretes with diode by Voltage, Current, Freq range"
-
-to_xmind.py datafiles\IGBT_Discretes.xlsx -i -o"15=IGBT + Diode" -t7 -t11 -t12 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_with_diode_Voltage_Freq_range.xmind --main="IGBT Discretes with diode by Voltage, Freq range"
-
-to_xmind.py datafiles\IGBT_Discretes.xlsx -i -o"15=IGBT" -t7 -t11 -t12 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_without_diode_Voltage_Freq_range.xmind --main="IGBT Discretes w/o diode by Voltage, Freq range"
-
-
-to_xmind.py datafiles\All_Automotive_PROFET.xlsx -i -t11 -t9 -t5 -t1 -u2 -a4 -a5 -a6 -a7 -a8 -a9 -a10 -a11 -a17 -a18 -a12 -a13 -a14 -a15 -a16 -a3 -n19 -n20 -n21 --xmind=datafiles\ALL_ATV_PROFET_By_Voltage_IL_Short_Circuit_Current_Num_of_Chan.xmind --main="Automotove PROFETs: Max Voltage, Short Circuit Current, # of Channels"
-
-
-to_xmind.py datafiles\IPM.xlsx -i -t9 -t12 -t13 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_P_mot.xmind --main="Voltage Class, Configuration, P_mot"
-
-to_xmind.py datafiles\IPM.xlsx -i -t9 -t12 -t10 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_Current.xmind --main="Voltage Class, Configuration, Current"
-
-to_xmind.py datafiles\IPM.xlsx -i -t9 -t12 -t14 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_R_ds(on).xmind --main="Voltage Class, Configuration, R_ds(on)"
+to_xmind.py datafiles\MOSFET.xlsx -i -d -e"9=P, N" -t9 -t10 -t13 -t17 -t5 -t1 -a10 -a11 -a12 -a13 -a14 -a15 -u2 -n16 -n18 -n4 --xmind=datafiles\MOSFET_Exotic_Types_By_Type_Voltage_R_dson.xmind --main="MOSFET Exotic Types By Type, Voltage, R_dson"
 
 
 
-to_xmind.py datafiles\Gate_Driver_ICs.xlsx -i -t5 -t11 -t15 -t10 -t1 -u2 -a16 -a35 -a13 -a15 -a12 -a4 -n5 -n6 -n7 -n8 -n9 -n10 -n11 -n12 -n13 -n14 -n15 -n16 -n17 -n18 -n19 -n20 -n21 -n22 -n23 -n24 -n25 -n26 -n27 -n28 -n29 -n30 -n31 -n32 -n33 -n34 -n35 -n36 -n37 -n38 -n39 -n40 -n41 -n42 -n43 -n44 -n45 -n46 -n47 --xmind=datafiles\Gate_Driver_ICs_Voltage_Configuration_SwitchType_Channels.xmind --main="Voltage, Configuration, Switch Type, # of Channels"
+to_xmind.py datafiles\IGBT_modules.xlsx -i -d -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_all_Voltage_Configuration_Current.xmind --main="All IGBT Modules by Voltage and Configuration"
+
+to_xmind.py datafiles\IGBT_modules.xlsx -i -d -o"8=Dual" -t6 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_Dual(FF)_Voltage_Current.xmind --main="Dual(FF) IGBT Modules by Voltage and Configuration"
+
+to_xmind.py datafiles\IGBT_modules.xlsx -i -d -o"8=Dual,Single" -t6 -t12 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_Dual(FF)_Single(FZ)_Voltage_Technology.xmind --main="Dual(FF), Single(FZ) IGBT Modules by Voltage and Technology"
+
+to_xmind.py datafiles\IGBT_modules.xlsx -i -d --match="9=.XT" -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_with_dot_XT.xmind --main="IGBT Modules with dot_XT"
+
+to_xmind.py datafiles\IGBT_modules.xlsx -i -d --match="15=Automotive" -t6 -t8 -t11 -t17 -t1 -a9 -a10 -a11 -a12 -a14 -n15 -n7 -n3 -u2 --xmind=datafiles\IGBT_Modules_for_Automotive_Voltage_Configuration_Current.xmind --main="IGBT Modules for Automotive by Voltage and Configuration"
+
+
+to_xmind.py datafiles\IGBT_Discretes.xlsx -i -d -o"15=IGBT" -t7 -t12 -t11 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_without_diode_Voltage_Current.xmind --main="IGBT Discretes w/o diode by Voltage, Current, Freq range"
+
+to_xmind.py datafiles\IGBT_Discretes.xlsx -i -d -o"15=IGBT + Diode" -t7 -t12 -t11 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_with_diode_Voltage_Current.xmind --main="IGBT Discretes with diode by Voltage, Current, Freq range"
+
+to_xmind.py datafiles\IGBT_Discretes.xlsx -i -d -o"15=IGBT + Diode" -t7 -t11 -t12 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_with_diode_Voltage_Freq_range.xmind --main="IGBT Discretes with diode by Voltage, Freq range"
+
+to_xmind.py datafiles\IGBT_Discretes.xlsx -i -d -o"15=IGBT" -t7 -t11 -t12 -t1 -u2 -a5 -a7 -a11 -a12 -a13 -a14 -a20 -a21 -a15 -a16 -a18 -n17 -n19 -n4 --xmind=datafiles\IGBT_Discretes_without_diode_Voltage_Freq_range.xmind --main="IGBT Discretes w/o diode by Voltage, Freq range"
+
+
+to_xmind.py datafiles\All_Automotive_PROFET.xlsx -i -d -t11 -t9 -t5 -t1 -u2 -a4 -a5 -a6 -a7 -a8 -a9 -a10 -a11 -a17 -a18 -a12 -a13 -a14 -a15 -a16 -a3 -n19 -n20 -n21 --xmind=datafiles\ALL_ATV_PROFET_By_Voltage_IL_Short_Circuit_Current_Num_of_Chan.xmind --main="Automotove PROFETs: Max Voltage, Short Circuit Current, # of Channels"
+
+
+to_xmind.py datafiles\IPM.xlsx -i -d -t9 -t12 -t13 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_P_mot.xmind --main="Voltage Class, Configuration, P_mot"
+
+to_xmind.py datafiles\IPM.xlsx -i -d -t9 -t12 -t10 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_Current.xmind --main="Voltage Class, Configuration, Current"
+
+to_xmind.py datafiles\IPM.xlsx -i -d -t9 -t12 -t14 -t8 -t1 -u2 -a5 -a9 -a10 -a11 -a12 -a13 -a14 -a4 -n6 -n7 --xmind=datafiles\IPM_by_Voltage_Configuration_R_ds(on).xmind --main="Voltage Class, Configuration, R_ds(on)"
 
 
 
-to_xmind.py datafiles\Thyristor_Discs.xlsx -i -o9="Electrical Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Housing_Voltage.xmind --main="Electrical Triggered, Housing, Voltage [V], I_TSM [A]"
-
-to_xmind.py datafiles\Thyristor_Discs.xlsx -i -o9="Electrical Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Voltage_Current_Housing.xmind --main="Electrical Triggered, Voltage [V], I_TSM Current [A]"
+to_xmind.py datafiles\Gate_Driver_ICs.xlsx -i -d -t5 -t11 -t15 -t10 -t1 -u2 -a16 -a35 -a13 -a15 -a12 -a4 -n5 -n6 -n7 -n8 -n9 -n10 -n11 -n12 -n13 -n14 -n15 -n16 -n17 -n18 -n19 -n20 -n21 -n22 -n23 -n24 -n25 -n26 -n27 -n28 -n29 -n30 -n31 -n32 -n33 -n34 -n35 -n36 -n37 -n38 -n39 -n40 -n41 -n42 -n43 -n44 -n45 -n46 -n47 --xmind=datafiles\Gate_Driver_ICs_Voltage_Configuration_SwitchType_Channels.xmind --main="Voltage, Configuration, Switch Type, # of Channels"
 
 
-to_xmind.py datafiles\Thyristor_Discs.xlsx -i -o9="Light Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Voltage_Current_Housing.xmind --main="Light Triggered, Voltage [V], I_TSM Current [A]"
 
-to_xmind.py datafiles\Thyristor_Discs.xlsx -i -o9="Light Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Housing_Voltage.xmind --main="Light Triggered, Housing, Voltage [V], I_TSM [A]"
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Electrical Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Housing_Voltage.xmind --main="Electrical Triggered, Housing, Voltage [V], I_TSM [A]"
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Electrical Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Voltage_Current_Housing.xmind --main="Electrical Triggered, Voltage [V], I_TSM Current [A]"
+
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Light Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Voltage_Current_Housing.xmind --main="Light Triggered, Voltage [V], I_TSM Current [A]"
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Light Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Housing_Voltage.xmind --main="Light Triggered, Housing, Voltage [V], I_TSM [A]"
+
+
+to_xmind.py datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder.xlsx -i -d -o10="Electrical Triggered Phase Control Thyristor" -t7 -t9 -t11 -t1 -u2 -a7 -a11 -a8 -a9 -a10 -n12 -n13 --xmind datafiles\Electrical_Triggered_Phase_Control_Thyristor_Discs.xmind --main="Electrical Triggered Phase Control Thyristor Discs"
+
+to_xmind.py datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder.xlsx -i -d -t7 -t10 -t9 -t11 -t1 -u2 -a7 -a11 -a8 -a9 -a10 -n12 -n13 --xmind datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder_Voltage_Configuration.xmind --main="Thyristor and Diode Discs"
+
+to_xmind.py datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder.xlsx -i -d -t10 -t7 -t9 -t11 -t1 -u2 -a7 -a11 -a8 -a9 -a10 -n12 -n13 --xmind datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder_Configuration_Voltage.xmind --main="Thyristor and Diode Discs"
+
+to_xmind.py datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder.xlsx -i -d -o10="Electrical Triggered Phase Control Thyristor" -t7 -t9 -t11 -t1 -u2 -a7 -a11 -a8 -a9 -a10 -n12 -n13 --xmind datafiles\Electrical_Triggered_Phase_Control_Thyristor_Discs.xmind --main="Electrical Triggered Phase Control Thyristor Discs"
+
+to_xmind.py datafiles\Thyristor_Diode_Discs_from_Bipolar_Disks_Finder.xlsx -i -d -o"10=Rectifier Diodes, Rectifier Diodes / Welding Diodes"  -t7 -t9 -t11 -t1 -u2 -a7 -a11 -a8 -a9 -a10 -n12 -n13 --xmind datafiles\Rectifier_Diodes_Discs.xmind --main="Rectifier and Welding Diode Discs"
+
+
 
---
+++ generate_thyristor_disks.bat
@@ -0,0 +1,8 @@
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Electrical Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Housing_Voltage.xmind --main="Electrical Triggered, Housing, Voltage [V], I_TSM [A]"
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Electrical Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Electrical_Triggered_By_Voltage_Current_Housing.xmind --main="Electrical Triggered, Voltage [V], I_TSM Current [A]"
+
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Light Triggered Phase Control Thyristor" -t3 -t11 -t8 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Voltage_Current_Housing.xmind --main="Light Triggered, Voltage [V], I_TSM Current [A]"
+
+to_xmind.py datafiles\Thyristor_Discs.xlsx -i -d -o9="Light Triggered Phase Control Thyristor" -t8 -t3 -t11 -t1 -u2 -a8 -a3 -a4 -a5 -a6 -a7 -a9 -a10 -a11 -a12 -a13 -a14 --xmind=datafiles\Thyristor_Disk_Light_Triggered_By_Housing_Voltage.xmind --main="Light Triggered, Housing, Voltage [V], I_TSM [A]"
